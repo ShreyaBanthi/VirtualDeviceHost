9ac53307783f34c51911803ecf92103d229b28ca
Nicolas Mehlei <nicolas.mehlei@gmail.com>
2018-07-12 11:09:55 +0200

    improved more or less everything

---
+++ VirtualValues/AggregatorStrategies/SimpleFloatSnapshotAggregatorStrategy.py
@@ -0,0 +1,20 @@
+from VirtualValues.AggregatorStrategy import AggregatorStrategy
+
+
+from Utilities import is_number
+
+'''
+class SimpleFloatSnapshotAggregatorStrategy(AggregatorStrategy):
+
+    def __init__(self):
+        pass
+
+    def aggregate(self, mappings):
+        values = []
+        for m in mappings:
+            raw_val = m.get_value()
+            if is_number(raw_val):
+                val = float(m.get_value())
+                values.append(val)
+        return values
+'''
---
+++ VirtualValues/SynthesisStrategies/AverageSyntStrategy.py
@@ -0,0 +1,11 @@
+from SyntStrategy import SyntStrategy
+
+'''
+class AverageSyntStrategy(SyntStrategy):
+    def __init__(self):
+        pass
+
+    def synthesize(self, values):
+        float_values = [float(i) for i in values]
+        return round(sum(float_values) / len(float_values), 2)
+'''
