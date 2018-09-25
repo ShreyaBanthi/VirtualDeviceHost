c081e17b6967c7a56ceb836266c2497686d61e76
Nicolas Mehlei <nicolas.mehlei@gmail.com>
2018-09-25 20:41:00 +0200

    optimized cli usage for background service scenarios

--- VDHApp.py
+++ VDHApp.py
@@ -8,20 +8,24 @@ class VDHApp:
         logging.basicConfig(level=logging.INFO)
         # logging.basicConfig(level=logging.DEBUG)
 
-        if len(sys.argv) != 2:
+        if len(sys.argv) < 2:
             logging.error('ERROR: Invalid configuration. Enter active configuration as command line argument.')
             return
 
         virtual_device_host = VirtualDeviceHost()
         logging.info('active configuration: ' + sys.argv[1])
         virtual_device_host.initialize(sys.argv[1])
-        # virtual_device_host.initialize('Scenario5ConfigurationStrategy')
 
         virtual_device_host.start()
 
         logging.info('VirtualDeviceHost now running')
         logging.info('Press ENTER to exit...')
-        input()
+        if len(sys.argv) >= 3 and sys.argv[2] == '-background':
+            while True:
+                pass
+        else:
+            logging.info('Press ENTER to exit...')
+            input()
 
         virtual_device_host.stop()
 
